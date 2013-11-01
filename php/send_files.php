7018e7b0474422393e69b5258f87020f22838543
tabio <tabio.github@gmail.com>
2013-11-01 15:01:28 +0900

    mod send_file.php

--- php/send_files.php
+++ php/send_files.php
@@ -1,11 +1,132 @@
 <?php
+/**
+ * send_files.php
+ * Send file by e-mail
+ * PHP version 5.4.14
+ * @link   http://github.com/tabio/common_work
+*/
 
+//---------------------------------------------------
+// Load pear modules
+// ==> pear install Mail
+// ==> pear install Mail_Mime
+// ==> pear install Net_SMTP
+//---------------------------------------------------
+require_once('Mail.php');
+require_once('Mail/mime.php');
+
+//***************************************************
+// Define
+//***************************************************
+define('MAIL_TO',        'hoge@hoge.co.jp');
+define('MAIL_TO_NAME',   'name for mail client');
+define('MAIL_FROM',      'hoge@hoge.co.jp');
+define('MAIL_FROM_NAME', 'name for mail client');
+define('SMTP_HOST',      'ssl://hoge.mail.jp');
+define('PORT',           465);
+define('SMTP_USER',      'hoge@hoge.co.jp');
+define('SMTP_PASS',      '123456789');
+
+
+//***************************************************
+// Methods
+//***************************************************
 function main($fpaths=array()) {
   try {
-    var_dump($fpaths);
+
+    // use japanese
+    mb_language('Japanese');
+    mb_internal_encoding('ISO-2022-JP');
+    mb_http_output('UTF-8');
+
+    // setting smtp params
+    $params = array(
+      'host'     => SMTP_HOST,
+      'port'     => PORT,
+      'auth'     => true,
+      'username' => SMTP_USER,
+      'password' => SMTP_PASS,
+      'timeout'  => 30
+    );
+
+    // subject
+    $subject = 'このメールはプログラムより送信されております。';
+    $subject = mb_convert_encoding($subject, 'ISO-2022-JP', 'auto');
+
+    // body
+    $body = 'このメールはプログラムより送信されております。';
+    $body = mb_convert_encoding($body, 'ISO-2022-JP', 'auto');
+    $mime_obj = new Mail_Mime("\n");
+    $mime_obj->setTxtBody($body);
+    
+    // add files
+    if ($fpaths) {
+      foreach($fpaths as $path) {
+        $mime_obj->addAttachment($path, get_mime_type($path));
+      }
+    }
+
+    $body_param = array(
+      'head_charset' => 'ISO-2022-JP',
+      'text_charset' => 'ISO-2022-JP'
+    );
+
+    $body = $mime_obj->get($body_param);
+
+    // set header
+    $add_headers = array(
+      'From'    => mb_encode_mimeheader(MAIL_FROM_NAME).'<'.MAIL_FROM.'>',
+      'To'      => mb_encode_mimeheader(MAIL_TO_NAME).  '<'.MAIL_TO.'>',
+      'Subject' => mb_encode_mimeheader($subject)
+    );
+    $headers = $mime_obj->headers($add_headers);
+
+    $mail = Mail::factory('smtp', $params);
+    if (PEAR::isError($mail->send(MAIL_TO, $headers, $body))) {
+      throw new Exception('failed to send mail');
+    }
+
   } catch(Exception $e) {
+    opt_msg(4, $e->getMessage());
   }
   return false;
+}
+
+function get_mime_type($name) {
+  $tmp = pathinfo($name);
+  switch ($tmp['extension']) {
+    case 'html':
+      $str = 'text/html';
+      break;
+    case 'xml':
+      $str = 'text/xml';
+      break;
+    case 'js':
+      $str = 'text/javascript';
+      break;
+    case 'css':
+      $str = 'text/css';
+      break;
+    case 'gif':
+      $str = 'image/gif';
+      break;
+    case 'jpg':
+    case 'jpeg':
+      $str = 'image/jpeg';
+      break;
+    case 'png':
+      $str = 'image/png';
+      break;
+    case 'doc':
+      $str = 'application/msword';
+      break;
+    case 'pdf':
+      $str = 'application/pdf';
+      break;
+    default:
+     $str = 'text/plain';
+  }
+  return $str;
 }
 
 function opt_msg($type=0,$msg='') {
@@ -18,32 +139,32 @@ function opt_msg($type=0,$msg='') {
   } elseif($type === 3) {
     echo "[warn]  not found => $msg\n";
   } elseif($type === 4) {
-    echo "[error] fail send mail\n";
+    echo "[error] fail send mail => $msg\n";
   }
 }
 
-if ($argc > 1) {
-  $targets  = array();
+//===================================================
+// start proguram
+//===================================================
+opt_msg(0);
+$targets  = array();
 
-  // start program
-  opt_msg(0);
-
-  for ($i=1;  $i<=$argc - 1; $i++) {
-    if (file_exists($argv[$i])) {
-      array_push($targets, $argv[$i]);
-    } else {
-      // file not found
-      opt_msg(3, $argv[$i]);
-    }
+// check files
+for ($i=1;  $i<=$argc - 1; $i++) {
+  if (file_exists($argv[$i])) {
+    array_push($targets, $argv[$i]);
+  } else {
+    // file not found
+    opt_msg(3, $argv[$i]);
   }
-
-  // send mail with target files
-  if ($targets) main($targets);
-
-  // end program
-  opt_msg(1);
-} else {
-  opt_msg(2);
 }
+
+// send mail
+main($targets);
+
+opt_msg(1);
+//===================================================
+// end proguram
+//===================================================
 
 ?>
